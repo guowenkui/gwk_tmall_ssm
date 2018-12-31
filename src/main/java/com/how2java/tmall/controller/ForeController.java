199587dfc299bd33f5a28bf1972f1b1eaa0debdb
郭文魁 <guowenkui@rongzi.com>
2018-12-31 22:13:44 +0800

    前台-无需登录--模态登录

--- src/main/java/com/how2java/tmall/controller/ForeController.java
+++ src/main/java/com/how2java/tmall/controller/ForeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpSession;
@@ -97,5 +98,29 @@ public class ForeController {
         model.addAttribute("reviews",reviews);
         model.addAttribute("pvs",pvs);
         return "fore/product";
+    }
+
+    @RequestMapping("forecheckLogin")
+    @ResponseBody
+    public String checkLogin(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        if (user!=null){
+            return "success";
+        }else {
+            return "fail";
+        }
+    }
+
+    @RequestMapping("foreloginAjax")
+    @ResponseBody
+    public String loginAjax(@RequestParam("name")String name,@RequestParam("password") String password,HttpSession session){
+        name = HtmlUtils.htmlEscape(name);
+        User user = this.userService.get(name,password);
+        if (user!=null){
+            session.setAttribute("user",user);
+            return "success";
+        }else {
+            return "fail";
+        }
     }
 }
